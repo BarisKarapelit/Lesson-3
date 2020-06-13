7f9f708c73cec856e82f5998a3b9695c9f313979
Barış Karapelit <baris_karapelit_298@hotmail.com>
2020-06-13 23:11:38 +0300

    End Lesson 3

--- firstCourse/app/src/main/java/com/example/firstcourse/MainActivity.java
+++ firstCourse/app/src/main/java/com/example/firstcourse/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.firstcourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        TextView  txt= (TextView)findViewById(R.id.txt);
+        txt.setText("Barıs,Burak");
     }
 }
